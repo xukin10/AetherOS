44e3cb9418f7f634aba561f5cf3f640dd80e58fa
xukin10 <2228677293@qq.com>
2026-07-02 22:39:29 +0800

    core(runtime): add sentinel errors

---
+++ core/runtime/errors.go
@@ -0,0 +1,10 @@
+package runtime
+
+import "errors"
+
+var (
+	ErrEngineNotFound          = errors.New("engine not found")
+	ErrEngineAlreadyRegistered = errors.New("engine already registered")
+	ErrRuntimeNotStarted       = errors.New("runtime not started")
+	ErrRuntimeAlreadyStarted   = errors.New("runtime already started")
+)
