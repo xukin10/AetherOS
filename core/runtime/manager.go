ef317577e355682f7dca1780f120195b7ef33fb4
xukin10 <2228677293@qq.com>
2026-07-02 23:00:16 +0800

    core(runtime): simplify Manager

--- core/runtime/manager.go
+++ core/runtime/manager.go
@@ -1,42 +1,7 @@
 package runtime
 
-import (
-	"sync"
-)
-
-type Manager struct {
-	mu      sync.RWMutex
-	engines map[string]Engine
-	order   []string
-}
+type Manager struct{}
 
 func NewManager() *Manager {
-	return &Manager{
-		engines: make(map[string]Engine),
-		order:   []string{},
-	}
-}
-
-func (m *Manager) Register(e Engine) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	name := e.Name()
-	if _, ok := m.engines[name]; ok {
-		return
-	}
-
-	m.engines[name] = e
-	m.order = append(m.order, name)
-}
-
-func (m *Manager) Engines() []Engine {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-
-	result := make([]Engine, 0, len(m.order))
-	for _, name := range m.order {
-		result = append(result, m.engines[name])
-	}
-	return result
+	return &Manager{}
 }
