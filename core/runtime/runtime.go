9fa52565cca20ca92ad0e17c05da83e0399a2deb
xukin10 <2228677293@qq.com>
2026-07-02 22:59:33 +0800

    core(runtime): simplify Runtime

--- core/runtime/runtime.go
+++ core/runtime/runtime.go
@@ -1,48 +1,7 @@
 package runtime
 
-import (
-	"context"
-)
-
-type Runtime struct {
-	manager   *Manager
-	lifecycle Lifecycle
-}
+type Runtime struct{}
 
 func NewRuntime() *Runtime {
-	return &Runtime{
-		manager:   NewManager(),
-		lifecycle: Created,
-	}
-}
-
-func (r *Runtime) Register(e Engine) {
-	r.manager.Register(e)
-}
-
-func (r *Runtime) Start(ctx context.Context) error {
-	r.lifecycle = Running
-
-	for _, e := range r.manager.Engines() {
-		if err := e.Start(ctx); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (r *Runtime) Stop(ctx context.Context) error {
-	r.lifecycle = Stopped
-
-	engines := r.manager.Engines()
-	for i := len(engines) - 1; i >= 0; i-- {
-		_ = engines[i].Stop(ctx)
-	}
-
-	return nil
-}
-
-func (r *Runtime) State() Lifecycle {
-	return r.lifecycle
+	return &Runtime{}
 }
